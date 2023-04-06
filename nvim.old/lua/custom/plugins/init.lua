1bce450292daa128eff46660f7d93c6c0d599c57
Abdul Rafay <99marafay@gmail.com>
2023-04-06 20:31:47 +0500

    updating config files

---
+++ nvim.old/lua/custom/plugins/init.lua
@@ -0,0 +1,17 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+{
+    'goolord/alpha-nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function ()
+        require'alpha'.setup(require'alpha.themes.startify'.config)
+    end
+}
+
+
+
+
+}
